bc087cc630f266fed0278f1d563174ff2a5cb35b
Vítor Gabriel <vggd@cin.ufpe.br>
2024-02-27 21:44:10 -0300

    create transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'value',
+        'category'
+    ];
+
+    /*
+    public function user() :BelongsTo
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+    */
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
--- database/migrations/2024_02_27_132505_create_transactions_table.php
+++ database/migrations/2024_02_27_132505_create_transactions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('type');
+            $table->float('value');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
@@ -23,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('transactions');
     }
 };
